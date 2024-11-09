19a81ed83fec71a9e2e0a173a7aa65d935a5b67c
csweeney02 <chase.g.sweeney@gmail.com>
2024-11-09 17:22:47 -0500

    added code to create hitbox for player and draw player, however player is not being drawn

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+
+    player = Player(x,y)
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill("black")
         pygame.display.flip()
-        
+        player.draw(screen)
+        clock.tick(60)
+        dt = clock.get_time()
+
 
 if __name__ == "__main__":
     main()
